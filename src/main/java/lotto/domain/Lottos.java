500b8cf609d91be92310058df744b89277d7401f
riroan <riroan@naver.com>
2024-04-03 15:18:12 +0900

    refactor: 클래스명과 동일한 필드명 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -5,17 +5,17 @@ import lotto.domain.Lotto;
 import java.util.List;
 
 public class Lottos {
-    private final List<Lotto> lottos;
+    private final List<Lotto> values;
 
-    public Lottos(List<Lotto> lottos) {
-        this.lottos = lottos;
+    public Lottos(List<Lotto> values) {
+        this.values = values;
     }
 
     public int getSize() {
-        return this.lottos.size();
+        return values.size();
     }
 
     public Lotto get(int ix) {
-        return lottos.get(ix);
+        return values.get(ix);
     }
 }
