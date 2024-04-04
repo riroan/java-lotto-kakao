3f2d78ef7a9bb69fc3cc95ca634b76d724654584
riroan <riroan@naver.com>
2024-04-04 11:56:44 +0900

    refactor: ballPool static으로 변경

--- src/main/java/lotto/generator/RandomLottoGenerator.java
+++ src/main/java/lotto/generator/RandomLottoGenerator.java
@@ -2,38 +2,34 @@ package lotto.generator;
 
 import lotto.domain.Ball;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RandomLottoGenerator {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int LENGTH = 6;
-    private final List<Ball> ballPool;
-
-    public RandomLottoGenerator() {
-        this.ballPool = new ArrayList<>();
-        IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
-                .forEach(number -> ballPool.add(new Ball(number)));
-    }
+    private final List<Ball> ballPool = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+            .mapToObj(Ball::new)
+            .collect(Collectors.toList());
 
     private List<Ball> generateNumbers() {
         Collections.shuffle(ballPool);
         return new ArrayList<>(ballPool.subList(0, RandomLottoGenerator.LENGTH));
     }
 
-    public Lottos generateLottos(long lottoCount) {
+    public List<Lotto> generateAutoLottos(long lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             Lotto lotto = generateLotto();
             lottos.add(lotto);
         }
 
-        return new Lottos(lottos);
+        return lottos;
     }
 
     public Lotto generateLotto() {
